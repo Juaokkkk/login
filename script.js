a62ba3a34fb5e45f151e013d3bb08af19729617b
Juaokkkk <117794205+Juaokkkk@users.noreply.github.com>
2023-02-15 07:18:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if(login == "jaoalfrdo@gmail.com" && senha == "joaoalfredo12"){
+    if(login == "admin@gmail.com" && senha == "admin123"){
         alert('sucesso');
         location.href = "landingpage.html"
     }
